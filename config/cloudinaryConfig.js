948b8f64a887dfc9aa37c3f77a018564149068eb
guswawan <agsn.does6@gmail.com>
2019-08-20 09:27:47 +0700

    make it batter again 27 please

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -1,4 +1,4 @@
-const cloudinary = require('cloudinary');
+const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
 const cloudinaryStorage = require('multer-storage-cloudinary');
 
