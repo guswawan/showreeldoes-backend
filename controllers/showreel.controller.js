68409671e02dea27e8264a3b6a2ae5bbb75adb07
guswawan <agsn.does6@gmail.com>
2019-08-20 09:13:52 +0700

    make it batter again 26 please

--- controllers/showreel.controller.js
+++ controllers/showreel.controller.js
@@ -29,7 +29,7 @@ module.exports = {
                     } else {
                         res.json({
                             success: true,
-                            results: result,
+                            result: result,
                             response: response,
                             message: "success"
                         })
